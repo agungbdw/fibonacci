66a63c3d89c2775b7ca7e5a2c60b540d3f71588b
Muh Agung Badawarman <agunblams@gmail.com>
2016-03-31 10:22:15 +0700

    Added files via upload

---
+++ jurnal1.java
@@ -0,0 +1,25 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String args[]) {
+    
+       
+        System.out.println("Masukkan Bilangan : ");
+        int number = new Scanner(System.in).nextInt();
+      
+        //printing Fibonacci series upto number
+        for(int i=1; i<=number; i++){
+            System.out.print(fibonacci(i) +" ");
+        }
+    } 
+ 
+    public static int fibonacci(int number){
+        if(number == 1 || number == 2){
+            return 1;
+        }
+        return fibonacci(number-1) + fibonacci(number -2); //tail recursion
+    }
+}
